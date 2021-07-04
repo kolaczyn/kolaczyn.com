3b670a4c727fde98d218f59481a2826e0f791444
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-07-04 22:39:14 +0200

    add custom scrollbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Lato&family=Roboto:wght@500&display=swap");
-html{
+html {
   scroll-behavior: smooth;
 }
 body {
@@ -31,10 +31,36 @@ h2 {
 h3 {
   @apply text-3xl;
 }
-h3, h4, h5, h6 {
+h3,
+h4,
+h5,
+h6 {
   @apply text-2xl;
 }
-*, *::before ,*::after {
+*,
+*::before,
+*::after {
   box-sizing: border-box;
+}
+/* nice scrollbar */
+::-webkit-scrollbar {
+  width: 12px;
+}
+html {
+  --scrollbar-track: white;
+  --scrollbar-thumb: #C6D7F9;
+  --scrollbar-hover: #98B7F4;
+}
+::-webkit-scrollbar-track {
+  background: var(--scrollbar-track);
+}
+::-webkit-scrollbar-thumb {
+  background: var(--scrollbar-thumb);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: var(--scrollbar-hover);
+}
+body {
+  overflow-y: scroll;
 }
 </style>
