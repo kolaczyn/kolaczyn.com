d0a362d3b5ff47a7cbbf2aa0829d99ae9f2ed4c0
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-07-04 22:29:30 +0200

    fix slightly broken layout

--- components/base/Footer.vue
+++ components/base/Footer.vue
@@ -1,13 +1,15 @@
 <template>
-  <LayoutContainerBig class="bg-peach-500 border-t-2 border-peach-600 py-4">
-    <ul class="flex sm:justify-around justify-between">
-      <li v-for="{url, label} in footerLinks" :key="label">
-        <BaseLink class="text-ink hover:text-ink-400" :href="url">
-          {{ label }}
-        </BaseLink>
-      </li>
-    </ul>
-  </LayoutContainerBig>
+  <div class="bg-peach border-t-2 border-peach-600 py-4">
+    <LayoutContainerBig>
+      <ul class="flex sm:justify-around justify-between">
+        <li v-for="{url, label} in footerLinks" :key="label">
+          <BaseLink class="text-ink hover:text-ink-400" :href="url">
+            {{ label }}
+          </BaseLink>
+        </li>
+      </ul>
+    </LayoutContainerBig>
+  </div>
 </template>
 
 <script>
