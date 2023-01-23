77307688ada5e2c65926778e55fe4a9a442e1035
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-23 23:58:42 +0100

    minify code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), svelte()],
+  integrations: [mdx(), tailwind(), svelte(), compress()]
 });
