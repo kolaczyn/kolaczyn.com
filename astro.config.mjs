750b67fa57e2a8c56288aeae8599ffd475d7bf52
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-23 23:58:37 +0100

    setup svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), svelte()],
 });
