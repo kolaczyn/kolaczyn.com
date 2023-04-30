c516cc0ca80138d65db082a98d34d60aa13dc50a
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-04-30 15:54:11 +0200

    optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,17 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import compress from "astro-compress";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), svelte(), compress()]
+  integrations: [
+    mdx(),
+    tailwind(),
+    svelte(),
+    compress(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
