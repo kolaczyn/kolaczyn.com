4fa67c2f335cd293bc2f71d6ec6901e5ba7d3f0a
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-04-30 11:58:14 +0200

    translate home page

--- src/i18n/sections.ts
+++ src/i18n/sections.ts
@@ -1,4 +1,18 @@
-type SectionNav = "nav.github" | "nav.code-review" | "nav.projects";
-type SectionFooter = "footer.copy" | "footer.why-copy";
+type Nav = "nav.github" | "nav.code-review" | "nav.projects";
+type Footer = "footer.copy" | "footer.why-copy";
+type Home = HomeFunFacts | HomeHeader | HomeSkillset;
 
-export type Section = SectionNav | SectionFooter;
+type HomeHeader = "home.h1-big" | "home.h1-small";
+
+type HomeFunFacts =
+  | "home.fun-facts.1"
+  | "home.fun-facts.2.outside-link"
+  | "home.fun-facts.2.inside-link"
+  | "home.fun-facts.3"
+  | "home.fun-facts.4"
+  | "home.fun-facts.5.outside-brackets"
+  | "home.fun-facts.5.inside-brackest";
+
+type HomeSkillset = "home.skillset.title" | "home.skillset.6";
+
+export type Section = Nav | Footer | Home;
--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -8,11 +8,38 @@ export const translations: Translations = {
 
     "footer.copy": "created by Paweł Kołaczyński",
     "footer.why-copy": "dunno what I'm adding a copyright, but why not :p",
+
+    "home.h1-big": "Hey, I'm Paweł, ",
+    "home.h1-small": "and here are a few fun facts about me:",
+    "home.fun-facts.1": "I'm a frontend developer specializing in React",
+    "home.fun-facts.2.outside-link": "I have a modest portfolio of websites, ",
+    "home.fun-facts.2.inside-link": "which you can see here",
+    "home.fun-facts.3": "I love learning about new frontend technologies",
+    "home.fun-facts.4": "Vim user",
+    "home.fun-facts.5.outside-brackets": "Functional programming enjoyer",
+    "home.fun-facts.5.inside-brackest": "proof",
+
+    "home.skillset.title": "My skillset",
+    "home.skillset.6": "Basics: Rust, Python, Go, Swift, C#",
   },
   pl: {
     "nav.projects": "Projekty",
 
     "footer.copy": "Paweł Kołaczyński",
     "footer.why-copy": "nie wiem czemu daję copyright, ale why not :p",
+
+    "home.h1-big": "Hej, jestem Paweł,",
+    "home.h1-small": "a oto parę słów o mnie:",
+    "home.fun-facts.1":
+      "Jestem frontend developerem specjalizującym się w React",
+    "home.fun-facts.2.outside-link": "Mam skromne portfolio stron, ",
+    "home.fun-facts.2.inside-link": "które możesz zobaczyć tutaj",
+    "home.fun-facts.3": "Uwielbiam poznawać nowe technologie frontendowe",
+    "home.fun-facts.4": "Vim-uję",
+    "home.fun-facts.5.outside-brackets": "Kocham programowanie funkcyjne",
+    "home.fun-facts.5.inside-brackest": "dowód",
+
+    "home.skillset.title": "Mój skillset",
+    "home.skillset.6": "Podstawy - Rust, Python, Go, C#",
   },
 };
