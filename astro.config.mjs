77c4e242f8ab4d5af72160387bd944dac9054a93
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-05-02 19:18:33 +0200

    add canonicals and redirect to current page in lang picker

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import compress from "astro-compress";
 import image from "@astrojs/image";
-
 import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx(),
