fef580129299027354d0fd4c731b7390214f653a
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-12-09 12:29:11 +0100

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,2 +1,4 @@
-//alert('hello from kolaczyn.com')
-document.querySelector('body').innerHTML = `<iframe width="1000px" height="500px" src="https://kolaczyn.com"/>`
+alert('This is XSS')
+document.body.style.background='red'
+//document.querySelector('body').innerHTML = `<iframe width="1000px" height="500px" src="https://kolaczyn.com"/>`
+// document.querySelector('body').innerHTML = `<iframe width="1000px" height="500px" src="https://kolaczyn.com"/>`
